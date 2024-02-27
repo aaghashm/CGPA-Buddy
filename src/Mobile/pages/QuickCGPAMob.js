9dfd6c85ae59e444c8bafd4e42e737d1fe185635
AAGHASH M <aaghashm@gmail.com>
2024-02-27 18:25:02 +0530

    Revert "multiple semester update"
    This reverts commit 6d4a61bfda0fd3e495004fc46fc10bf6c079242b.

--- src/Mobile/pages/QuickCGPAMob.js
+++ src/Mobile/pages/QuickCGPAMob.js
@@ -1,35 +1,29 @@
-import React, { useState, useEffect } from "react";
-import { Checkbox } from "@chakra-ui/react";
+import React, { useState,useEffect } from "react";
+import { Select } from "@chakra-ui/react";
 import HEADERMOB from "../components/HeaderMob";
 import FOOTERFRAME from "../components/FooterFrame";
 import "./QuickCGPAMob.css";
 import CustomAlert from "../../components/CustomAlert";
 import departments from "../../departments";
-import { Select } from "@chakra-ui/react";
 
 const QuickCGPAMob = () => {
-  const [selectedSemesters, setSelectedSemesters] = useState([]); // State for selected semesters
+  const [semester, setSemester] = useState("1");
   const [department, setDepartment] = useState("CSE & ALLIED BRANCHES");
   const [grades, setGrades] = useState({});
-  const [alertIsOpen, setAlertIsOpen] = useState(false);
-  const [alertHeader, setAlertHeader] = useState("");
-  const [alertText, setAlertText] = useState("");
+  const [alertIsOpen, setAlertIsOpen] = useState(false); // State for controlling CustomAlert
+  const [alertHeader, setAlertHeader] = useState(""); // State for alert header
+  const [alertText, setAlertText] = useState(""); 
   const [selectedSubject, setSelectedSubject] = useState(null);
-
+  // Define subjects, credits, and departments
+  
   const handleSemesterChange = (e) => {
-    const sem = e.target.value;
-    setSelectedSemesters((prevSemesters) => {
-      if (prevSemesters.includes(sem)) {
-        return prevSemesters.filter((s) => s !== sem);
-      } else {
-        return [...prevSemesters, sem];
-      }
-    });
+    setSemester(e.target.value);
   };
 
   const handleClickSubject = (subjectData) => {
     setSelectedSubject(subjectData);
-    const alertMessage = `Name: ${subjectData.subject}\nCredits: ${subjectData.credit}\nDepartment: ${department}\nSemester(s): ${selectedSemesters.join(", ")}`;
+    // Construct alert message
+    const alertMessage = `Name: ${subjectData.subject}\nCredits: ${subjectData.credit}\nDepartment: ${department}\nSemester: ${semester}`;
     openAlert("Subject Details", alertMessage);
   };
 
@@ -38,17 +32,16 @@ const QuickCGPAMob = () => {
   };
 
   useEffect(() => {
-    // Reset grades when department changes
+    // Reset grades when semester or department changes
     setGrades({});
-  }, [department]);
+  }, [semester, department]);
 
   const handleGradeChange = (subject, grade) => {
     setGrades((prevGrades) => ({
       ...prevGrades,
-      [subject]: grade,
+      [subject]: grade
     }));
   };
-
   const openAlert = (header, text) => {
     setAlertHeader(header);
     setAlertText(text);
@@ -58,53 +51,49 @@ const QuickCGPAMob = () => {
   const closeAlert = () => {
     setAlertIsOpen(false);
   };
-
   const calculateCGPA = () => {
-    event.preventDefault(); 
-    let subjects = [];
-    selectedSemesters.forEach((sem) => {
-      subjects = subjects.concat(departments[department].semesters[sem]);
-    });
-
+    event.preventDefault();
+    const subjects = departments[department].semesters[semester];
     let totalCredits = 0;
     let totalGradePoints = 0;
     let failedSubjects = [];
-
-    subjects.forEach((subjectData) => {
+  
+    for (const subjectData of subjects) {
       const { subject, credit } = subjectData;
       const grade = grades[subject];
-
+  
       if (grade === undefined || grade === -1) {
         console.error(`Grade for ${subject} not provided.`);
-        openAlert("OOPS!", `Please choose a grade for ${subject}.`);
+        openAlert("OOPS!",`Please choose a grade for ${subject}.`);
         return;
       }
-
-      if (grade > 100) {
+  
+      if (grade>100) {
         failedSubjects.push(subject);
-        return; // Skip failed subjects from calculation
+        continue; // Skip failed subjects from calculation
       }
-
+  
       totalCredits += credit;
       totalGradePoints += credit * grade;
-    });
-
+    }
+  
     if (totalCredits === 0) {
-      openAlert("OOPS!", "No subjects found for calculation.");
+      openAlert("OOPS!","No subjects found for calculation.");
       return;
     }
-
+  
     const cgpa = totalGradePoints / totalCredits;
     const failedCount = failedSubjects.length;
     let alertMessage = `CGPA: ${cgpa.toFixed(2)}\n`;
-
+    
     if (failedCount > 0) {
       alertMessage += `No of Arrears: ${failedCount}\n`;
-      alertMessage += `Arrear Subject Names:\n ${failedSubjects.join(",\n ")}\n`;
+      alertMessage += `Arrear Subject Names: ${failedSubjects.join(", ")}\n`;
     }
-
+  
     openAlert("Here you go!", alertMessage);
   };
+  
 
   return (
     <div className="quick-cgpa-calculator">
@@ -114,128 +103,105 @@ const QuickCGPAMob = () => {
         word3="CALCULATOR"
         image="Quick Calculate.png"
       />
-      <CustomAlert
-        isOpen={alertIsOpen}
-        onClose={() => setAlertIsOpen(false)}
-        header={alertHeader}
-        alertText={alertText}
-      />
+ <CustomAlert isOpen={alertIsOpen} onClose={() => setAlertIsOpen(false)} header={alertHeader} alertText={alertText} />
 
-      <div className="quick-cgpa-calculator" style={{ gap: "0px", paddingBottom: "10px", backgroundColor: "#f2f4f7" }}>
+      <div className="quick-cgpa-calculator" style={{gap:"0px",paddingBottom:"10px",backgroundColor:"#f2f4f7"}}>
 
         <div className="depsem">
-          <div className="department-selection-frame">
-            <h3 className="semester">Semester</h3>
-            {[1, 2, 3].map((sem) => (
-              <Checkbox
-                size='lg'
-                colorScheme='green'
-                key={sem}
-                isChecked={selectedSemesters.includes(sem.toString())}
-                onChange={handleSemesterChange}
-                value={sem}
-                style={{ backgroundColor: 'white', borderColor: 'black', fontSize: '16px' }} // Set background color to white and border color to black
-              >
-                <span style={{ backgroundColor: 'transparent', fontSize: '16px' }}>{sem}</span> {/* Set text background color to transparent */}
-              </Checkbox>
+       <div className="department-selection-frame">
+          <h3 className="semester">Semester</h3>
+          <Select
+            className="select-semester"
+            backgroundColor="#F2F4F7"
+            value={semester}
+            onChange={handleSemesterChange}
+          >
+            {Object.keys(departments[department].semesters).map((sem) => (
+              <option key={sem} value={sem}>
+                {sem}
+              </option>
             ))}
-          </div>
-
-          <div className="semester-selection-frame">
-            <h3 className="department">Department</h3>
-            <Select
-              className="select-department"
-              backgroundColor="#F2F4F7"
-              value={department}
-              onChange={handleDepartmentChange}
-            >
-              {Object.keys(departments).map((dept) => (
-                <option key={dept} value={dept}>
-                  {dept}
-                </option>
-              ))}
-            </Select>
-          </div>
+          </Select>
         </div>
-        <section className="form-body-wrapper">
-          {!selectedSemesters.length ||
-            selectedSemesters.every(
-              (sem) => !departments[department].semesters[sem]
-            ) ? (
-            <form className="form-body">
-              <b className="form-title">Data Is Unavailable</b>
-              <div className="subname">Kindly contact aaghashm@gmail.com</div>
-            </form>
-          ) : (
-            <form className="form-body">
-              <div className="form-container" />
-              <div className="subjects-list">
-                <b className="form-title">Enter Grades for Respective Subjects</b>
-                {selectedSemesters.flatMap((sem) =>
-                  departments[department].semesters[sem].map((subjectData, index) => (
-                    <div
-                      key={`${sem}-${index}`}
-                      className={`subject ${selectedSubject === subjectData ? 'selected' : ''}`}
-                    >
-                      <div className="subject-item" />
-                      <div className="subcover">
-                        <div
-                          className="subname"
-                          onClick={() => handleClickSubject(subjectData)}
-                        >
-                          {subjectData.subject}
-                        </div>
-                      </div>
-                      <div className="group-select-container">
-                        <Select
-                          iconSize={28}
-                          className="group-select"
-                          w="100px"
-                          backgroundColor="#F2F4F7"
-                          onChange={(e) =>
-                            handleGradeChange(
-                              subjectData.subject,
-                              parseInt(e.target.value)
-                            )
-                          }
-                          required
-                          value={grades[subjectData.subject] || -1}
-                        >
-                          <option value={-1} disabled>
-                            --Select Grade--
-                          </option>
-                          <option value={10}>10 (O)</option>
-                          <option value={9}>9 (A+)</option>
-                          <option value={8}>8 (A)</option>
-                          <option value={7}>7 (B+)</option>
-                          <option value={6}>6 (B)</option>
-                          <option value={5}>5 (C+)</option>
-                          <option value={4}>4 (C)</option>
-                          <option value={999}>0 (U) -- Arrear</option>
-                        </Select>
-                      </div>
+        <div className="semester-selection-frame">
+          <h3 className="department">Department</h3>
+          <Select
+            className="select-department"
+            backgroundColor="#F2F4F7"
+            value={department}
+            onChange={handleDepartmentChange}
+          >
+            {Object.keys(departments).map((dept) => (
+              <option key={dept} value={dept}>
+                {dept}
+              </option>
+            ))}
+          </Select>
+        </div>
+        </div>
+      <section className="form-body-wrapper">
+      {(!departments[department].semesters[semester] || departments[department].semesters[semester].length === 0) ? (
+        <form className="form-body">
+            <b className="form-title">Data Is Unavailable</b>
+            <div className="subname">Kindly contact aaghashm@gmail.com</div>
+        </form>
+      ) : (
+    <form className="form-body">
+      <div className="form-container" />
+      <div className="subjects-list">
+        <b className="form-title">Enter Grades for Respective Subjects</b>
+        {departments[department].semesters[semester].map((subjectData, index) => (
+                    <div key={index} className={`subject ${selectedSubject === subjectData ? 'selected' : ''}`} >
+
+            <div className="subject-item" />
+            <div className="subcover">
+                        <div className="subname" onClick={() => handleClickSubject(subjectData)}>{subjectData.subject}</div></div>
+                                                <div className="group-select-container">
+                                                <Select
+                                                    className="group-select"
+                                                    w="100px" 
+                                                    backgroundColor="#F2F4F7"
+                                                    onChange={(e) => handleGradeChange(subjectData.subject, parseInt(e.target.value))}
+                                                    required
+                                                    value={grades[subjectData.subject] || -1} 
+                                                >
+                                                    <option value= {-1} disabled>
+                                                    --Select Grade--
+                                                    </option>
+                                                    <option value={10}>10 (O)</option>
+                                                    <option value={9}>9 (A+)</option>
+                                                    <option value={8}>8 (A)</option>
+                                                    <option value={7}>7 (B+)</option>
+                                                    <option value={6}>6 (B)</option>
+                                                    <option value={5}>5 (C+)</option>
+                                                    <option value={4}>4 (C)</option>
+                                                    <option value={999}>0 (U) --Arrear</option>
+                                                </Select>
+                                                
+                                                </div>
                     </div>
-                  ))
-                )}
-              </div>
-              <button className="CALCULATE-NOW-BUTTON1" onClick={calculateCGPA} >
-                <div className="ANALYZE-NOW-BUTTON-FRAME">
-                  <b className="QCB-BODY">CALCULATE NOW</b>
-                </div>
-                <img
-                  className="CALCULATE-NOW-BUTTON-CHILD"
-                  alt=""
-                  src="/arrow-1.svg"
-                />
-              </button>
-            </form>
-          )}
-        </section>
+                 
+        ))}
       </div>
+      <button className="CALCULATE-NOW-BUTTON1" onClick={calculateCGPA} >
+            <div className="ANALYZE-NOW-BUTTON-FRAME">
+              <b className="QCB-BODY">CALCULATE NOW</b>
+            </div>
+            <img
+              className="CALCULATE-NOW-BUTTON-CHILD"
+              alt=""
+              src="/arrow-1.svg"
+            />
+          </button>
+    </form>
+  )}
+      </section>
+          </div>
 
       <FOOTERFRAME />
     </div>
   );
 };
+
 
 export default QuickCGPAMob;
--- src/pages/QuickCGPACalculator.js
+++ src/pages/QuickCGPACalculator.js
@@ -1,35 +1,30 @@
-import React, { useState, useEffect } from "react";
-import { Checkbox } from "@chakra-ui/react"; // Import Checkbox from Chakra UI
+import React, { useState,useEffect } from "react";
+import { Select } from "@chakra-ui/react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import "./QuickCGPACalculator.css";
 import CustomAlert from "../components/CustomAlert";
 import departments from "../departments";
-import { Select } from "@chakra-ui/react";
 
 const QuickCGPACalculator = () => {
-  const [selectedSemesters, setSelectedSemesters] = useState([]); // State for selected semesters
+  const [semester, setSemester] = useState("1");
   const [department, setDepartment] = useState("CSE & ALLIED BRANCHES");
   const [grades, setGrades] = useState({});
-  const [alertIsOpen, setAlertIsOpen] = useState(false);
-  const [alertHeader, setAlertHeader] = useState("");
-  const [alertText, setAlertText] = useState("");
-  const [selectedSubject, setSelectedSubject] = useState(null);
+  const [alertIsOpen, setAlertIsOpen] = useState(false); // State for controlling CustomAlert
+  const [alertHeader, setAlertHeader] = useState(""); // State for alert header
+  const [alertText, setAlertText] = useState(""); 
+  const [selectedSubject, setSelectedSubject] = useState(null); // State for tracking selected subject
+  // Define subjects, credits, and departments
+
 
   const handleSemesterChange = (e) => {
-    const sem = e.target.value;
-    setSelectedSemesters((prevSemesters) => {
-      if (prevSemesters.includes(sem)) {
-        return prevSemesters.filter((s) => s !== sem);
-      } else {
-        return [...prevSemesters, sem];
-      }
-    });
+    setSemester(e.target.value);
   };
 
   const handleClickSubject = (subjectData) => {
     setSelectedSubject(subjectData);
-    const alertMessage = `Name: ${subjectData.subject}\nCredits: ${subjectData.credit}\nDepartment: ${department}\nSemester(s): ${selectedSemesters.join(", ")}`;
+    // Construct alert message
+    const alertMessage = `Name: ${subjectData.subject}\nCredits: ${subjectData.credit}\nDepartment: ${department}\nSemester: ${semester}`;
     openAlert("Subject Details", alertMessage);
   };
 
@@ -38,17 +33,16 @@ const QuickCGPACalculator = () => {
   };
 
   useEffect(() => {
-    // Reset grades when department changes
+    // Reset grades when semester or department changes
     setGrades({});
-  }, [department]);
+  }, [semester, department]);
 
   const handleGradeChange = (subject, grade) => {
     setGrades((prevGrades) => ({
       ...prevGrades,
-      [subject]: grade,
+      [subject]: grade
     }));
   };
-
   const openAlert = (header, text) => {
     setAlertHeader(header);
     setAlertText(text);
@@ -58,54 +52,51 @@ const QuickCGPACalculator = () => {
   const closeAlert = () => {
     setAlertIsOpen(false);
   };
-
   const calculateCGPA = () => {
-    let subjects = [];
-    selectedSemesters.forEach((sem) => {
-      subjects = subjects.concat(departments[department].semesters[sem]);
-    });
-
+    const subjects = departments[department].semesters[semester];
     let totalCredits = 0;
     let totalGradePoints = 0;
     let failedSubjects = [];
-
-    subjects.forEach((subjectData) => {
+  
+    for (const subjectData of subjects) {
       const { subject, credit } = subjectData;
       const grade = grades[subject];
-
+  
       if (grade === undefined || grade === -1) {
         console.error(`Grade for ${subject} not provided.`);
-        openAlert("OOPS!", `Please choose a grade for ${subject}.`);
+        openAlert("OOPS!",`Please choose a grade for ${subject}.`);
         return;
       }
-
-      if (grade > 100) {
+  
+      if (grade>100) {
         failedSubjects.push(subject);
-        return; // Skip failed subjects from calculation
+        continue; // Skip failed subjects from calculation
       }
-
+  
       totalCredits += credit;
       totalGradePoints += credit * grade;
-    });
-
+    }
+  
     if (totalCredits === 0) {
-      openAlert("OOPS!", "No subjects found for calculation.");
+      openAlert("OOPS!","No subjects found for calculation.");
       return;
     }
-
+  
     const cgpa = totalGradePoints / totalCredits;
     const failedCount = failedSubjects.length;
     let alertMessage = `CGPA: ${cgpa.toFixed(2)}\n`;
-
+    
     if (failedCount > 0) {
       alertMessage += `No of Arrears: ${failedCount}\n`;
       alertMessage += `Arrear Subject Names:\n ${failedSubjects.join(",\n ")}\n`;
     }
-
+  
     openAlert("Here you go!", alertMessage);
   };
+  
 
   return (
+
     <div className="quick-cgpa-calculator">
       <Header
         titleWord1="QUICK"
@@ -113,29 +104,23 @@ const QuickCGPACalculator = () => {
         titleWord3="CALCULATOR"
         image="Quick Calculate.png"
       />
-      <CustomAlert
-        isOpen={alertIsOpen}
-        onClose={() => setAlertIsOpen(false)}
-        header={alertHeader}
-        alertText={alertText}
-      />
+       <CustomAlert isOpen={alertIsOpen} onClose={() => setAlertIsOpen(false)} header={alertHeader} alertText={alertText} />
       <div className="semester-selection-frame">
-      <div className="department-selection-frame">
-  <h3 className="semester">Semester</h3>
-  {[1, 2, 3].map((sem) => (
-    <Checkbox
-    size='lg' colorScheme='green'
-      key={sem}
-      isChecked={selectedSemesters.includes(sem.toString())}
-      onChange={handleSemesterChange}
-      value={sem}
-      style={{ backgroundColor: 'transparent', borderColor: 'black' ,fontSize: '16px'}} // Set background color to white and border color to black
-    >
-      <span style={{ backgroundColor: 'transparent' ,fontSize: '16px'}}>{sem}</span> {/* Set text background color to transparent */}
-    </Checkbox>
-  ))}
-</div>
-
+        <div className="department-selection-frame">
+          <h3 className="semester">Semester</h3>
+          <Select
+            className="select-semester"
+            backgroundColor="#F2F4F7"
+            value={semester}
+            onChange={handleSemesterChange}
+          >
+            {Object.keys(departments[department].semesters).map((sem) => (
+              <option key={sem} value={sem}>
+                {sem}
+              </option>
+            ))}
+          </Select>
+        </div>
         <div className="semester-selection-frame">
           <h3 className="department">Department</h3>
           <Select
@@ -153,86 +138,62 @@ const QuickCGPACalculator = () => {
         </div>
       </div>
       <section className="form-body-wrapper">
-        {!selectedSemesters.length ||
-        selectedSemesters.every(
-          (sem) => !departments[department].semesters[sem]
-        ) ? (
-          <form className="form-body">
+      {(!departments[department].semesters[semester] || departments[department].semesters[semester].length === 0) ? (
+        <form className="form-body">
             <b className="form-title">Data Is Unavailable</b>
             <div className="subname">Kindly contact aaghashm@gmail.com</div>
-          </form>
-        ) : (
-          <form className="form-body">
-            <div className="form-container" />
-            <div className="subjects-list">
-              <b className="form-title">
-                Enter Grades for Respective Subjects
-              </b>
-              {selectedSemesters.flatMap((sem) =>
-                departments[department].semesters[sem].map((subjectData, index) => (
-                  <div
-                    key={`${sem}-${index}`}
-                    className={`subject ${
-                      selectedSubject === subjectData ? "selected" : ""
-                    }`}
-                  >
-                    <div className="subject-item" />
-                    <div className="subcover">
-                      <div
-                        className="subname"
-                        onClick={() => handleClickSubject(subjectData)}
-                      >
-                        {subjectData.subject}
-                      </div>
-                    </div>
-                    <div className="group-select-container">
-                      <Select
-                        iconSize={28}
-                        className="group-select"
-                        w="179px"
-                        backgroundColor="#F2F4F7"
-                        onChange={(e) =>
-                          handleGradeChange(
-                            subjectData.subject,
-                            parseInt(e.target.value)
-                          )
-                        }
-                        required
-                        value={grades[subjectData.subject] || -1}
-                      >
-                        <option value={-1} disabled>
-                          --Select Grade--
-                        </option>
-                        <option value={10}>10 (O)</option>
-                        <option value={9}>9 (A+)</option>
-                        <option value={8}>8 (A)</option>
-                        <option value={7}>7 (B+)</option>
-                        <option value={6}>6 (B)</option>
-                        <option value={5}>5 (C+)</option>
-                        <option value={4}>4 (C)</option>
-                        <option value={999}>0 (U) -- Arrear</option>
-                      </Select>
-                    </div>
-                  </div>
-                ))
-              )}
+        </form>
+      ) : (
+    <form className="form-body">
+      <div className="form-container" />
+      <div className="subjects-list">
+        <b className="form-title">Enter Grades for Respective Subjects</b>
+        {departments[department].semesters[semester].map((subjectData, index) => (
+          <div key={index} className={`subject ${selectedSubject === subjectData ? 'selected' : ''}`} >
+            <div className="subject-item" />            
+            <div className="subcover">
+            <div className="subname" onClick={() => handleClickSubject(subjectData)} >{subjectData.subject}</div></div>
+            <div className="group-select-container">
+              <Select
+              iconSize={28}
+                className="group-select"
+                w="179px"
+                backgroundColor="#F2F4F7"
+                onChange={(e) => handleGradeChange(subjectData.subject, parseInt(e.target.value))}
+                required
+                value={grades[subjectData.subject] || -1}
+              >
+                <option value= {-1} disabled>
+                  --Select Grade--
+                </option>
+                <option value={10}>10 (O)</option>
+                <option value={9}>9 (A+)</option>
+                <option value={8}>8 (A)</option>
+                <option value={7}>7 (B+)</option>
+                <option value={6}>6 (B)</option>
+                <option value={5}>5 (C+)</option>
+                <option value={4}>4 (C)</option>
+                <option value={999}>0 (U) -- Arrear</option>
+              </Select>
             </div>
-            <div className="analuse-button-wrapper">
-              <div className="analuse-button7" onClick={calculateCGPA}>
-                <b className="calculate-now">CALCULATE NOW</b>
-                <img
-                  className="analuse-button-child3"
-                  alt=""
-                  src="/arrow-1.svg"
-                />
-              </div>
-            </div>
-          </form>
-        )}
-      </section>
+          </div>
+        ))}
+      </div>
+      <div className="analuse-button-wrapper">
+        <div className="analuse-button7" onClick={calculateCGPA}>
+          <b className="calculate-now">CALCULATE NOW</b>
+          <img className="analuse-button-child3" alt="" src="/arrow-1.svg" />
+        </div>
+      </div>
+    </form>
+  )}
+</section>
+
+
       <Footer />
     </div>
   );
 };
+
 
 export default QuickCGPACalculator;
